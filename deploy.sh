ee1166f551124f52e83351e1c54739ff90b3c6c6
Maciel Oliveira <elitymaciel@hotmail.com>
2026-03-14 13:58:56 -0300

    adiciona pausa no script de deploy e ajusta configuração do Nginx para uso de variável

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ docker compose -f docker-compose.production.yml exec app php artisan config:cach
 docker compose -f docker-compose.production.yml exec app php artisan route:cache
 docker compose -f docker-compose.production.yml exec app php artisan view:cache
 docker compose -f docker-compose.production.yml exec app php artisan optimize
-
+sleep 10
 # Opcional: restart queue workers se usar Horizon/Queue
 # php artisan queue:restart
 
