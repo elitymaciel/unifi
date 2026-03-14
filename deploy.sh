ee3daf47bbea62eac44aaf49add2838ccbfcb065
Maciel Oliveira <elitymaciel@hotmail.com>
2026-03-14 11:27:27 -0300

    ajuste na instalação do laravel

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,12 @@ docker compose -f docker-compose.production.yml down --remove-orphans
 docker compose -f docker-compose.production.yml pull
 docker compose -f docker-compose.production.yml up -d --build  # --build só se tiver build local
 
+docker compose -f docker-compose.production.yml exec app php artisan migrate --force
+docker compose -f docker-compose.production.yml exec app php artisan config:cache
+docker compose -f docker-compose.production.yml exec app php artisan route:cache
+docker compose -f docker-compose.production.yml exec app php artisan view:cache
+docker compose -f docker-compose.production.yml exec app php artisan optimize
+
 # Opcional: restart queue workers se usar Horizon/Queue
 # php artisan queue:restart
 
